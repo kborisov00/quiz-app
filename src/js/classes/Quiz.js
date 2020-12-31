a18054d18c9329167f16e3f1ad5e0a29357dde00
kgenkov <kborisoph00@gmail.com>
2020-12-31 12:03:51 +0200

    starting quiz class from scratch

--- src/js/classes/Quiz.js
+++ src/js/classes/Quiz.js
@@ -1,2 +1,13 @@
 export default class Quiz {
+  constructor(config) {
+    this.questions = config.questions;
+    this.questionNode = config.questionNode;
+    this.quizAnswersNode = config.quizAnswersNode;
+  }
+
+  // startQuiz() {}
+
+  // previousQuestion() {}
+
+  // nextQuestion() {}
 }
